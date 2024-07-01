4d306e9d405e4271fdfd71dfe87afc8f450df3e3
Christian Finel <macreiben@gmail.com>
2024-07-01 23:46:16 +0200

    use case 1 and 2 added.

---
+++ src/FizzBuzzEnterprise/FizzBuzzBusiness.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.Design;
+using NFluent;
+
+namespace FizzBuzzBusiness.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GIVEN_1_THEN_1()
+    {
+        var original = 1;
+
+        var result = "1";
+
+        Check.That(result).IsEqualTo("1");
+    }
+    
+    [Fact]
+    public void GIVEN_2_THEN_2()
+    {
+        var original = 1;
+
+        var result = "1";
+
+        Check.That(result).IsEqualTo("1");
+    }
+}
